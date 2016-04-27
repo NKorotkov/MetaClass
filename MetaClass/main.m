fecace4946cae4d0c00356e4ec5785b732c9a927
NKorotkov <korotkov.nk@gmail.com>
2016-04-27 16:29:37 +0300

    Initial Commit

---
+++ MetaClass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MetaClass
+//
+//  Created by Nikolay Korotkov on 27/04/16.
+//  Copyright © 2016 Nikolay Korotkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
